be38397437a70acfb5fd6a8f9581f264dd326032
Cinta Adhiningrat <cintaamail@gmail.com>
2020-07-28 14:30:22 -0700

    Initial Commit

---
+++ Github_Trial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github_Trial
+//
+//  Created by Cinta Adhiningrat on 7/28/20.
+//  Copyright © 2020 Cinta Adhiningrat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
